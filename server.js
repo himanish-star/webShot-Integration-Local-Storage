ef8c4532789ae1b49ce4aae17b5320a18ab7d2b9
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2017-12-09 17:50:51 +0530

    changes made

--- server.js
+++ server.js
@@ -4,25 +4,18 @@ const app = express();
 
 
 let webshot = require('webshot');
-
 let specificData;
 
-//home Page
 app.get('/',(req,res)=>{
     res.send("let's get started");
 });
 
-//page for viewing Image
 app.get('/imageViewer',(req,res)=>{
-    res.sendFile('/home/soumya/WebstormProjects/webshot/index.html');
+    res.sendFile(__dirname+'/index.html');
 });
 
-//sending image for trial usage
-app.use('/use',express.static('/home/soumya/WebstormProjects/webshot/google.com.png'));
-
-//used for storing to local storage
 app.get('/storeImageToLocalStorage',(req,res,next)=>{
-    let renderStream = webshot('google.com');
+    let renderStream = webshot('stackoverflow.com');
     renderStream.on('data', function(data) {
         specificData=data.toString('base64');
         next();
